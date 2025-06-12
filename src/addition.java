9df8f5a05561fa8b760aaedff7db785269deaed4
mahima7080 <mahimasrivastava84@gmail.com>
2025-06-12 11:40:15 +0530

    edit addition

--- src/addition.java
+++ src/addition.java
@@ -2,15 +2,22 @@ public class addition {
 
 
     public static void main(String[] args)
-    {
+    {/**
+    you can also write int r = aa.add(5,9); , and then priny r ;
+
+     or in add function you can directly pass a+b instead of passing var int result= a+b
+
+
+    */
         addition aa=new addition();
-        aa.add(5,9);
+        //int r=aa.add(5,9);
+        System.out.println(aa.add(5,9));
     }
 
-     void add(int a,int b)
+     int add(int a,int b)
     {
-        int result=a+b;
-        System.out.println(result);
+        //int result=a+b;
+        return a+b;
     }
 }
 
