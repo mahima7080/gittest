934f468fc02e80f7ed4f6e84e4782a6780be01b0
mahima7080 <mahimasrivastava84@gmail.com>
2025-06-12 09:55:17 +0530

    third

--- src/Ternary.java
+++ src/Ternary.java
@@ -3,8 +3,10 @@ public class Ternary {
     public static void main(String[] args)
     {
         int a=10, b=11 ,c=9;
-        int min= (a<b)? a: (b>c)?b:c;
+        int min= a<b? a: b>c?b:c;
         System.out.println(min);
+        System.out.println(min);
+
 
     }
 
