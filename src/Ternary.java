7b624c18aa93bfb1cb4018463eb16fd976c11994
mahima7080 <mahimasrivastava84@gmail.com>
2025-06-12 09:46:40 +0530

    ternary

---
+++ src/Ternary.java
@@ -0,0 +1,11 @@
+public class Ternary {
+
+    public static void main(String[] args)
+    {
+        int a=10, b=11 ,c=9;
+        int max= (a>b)? a: (b>c)?b:c;
+        System.out.println(max);
+
+    }
+
+}
