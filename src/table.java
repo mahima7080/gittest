c5225888f3df3c3cfc9c92585ad84f438cb5bc0f
mahima7080 <mahimasrivastava84@gmail.com>
2025-06-12 12:00:35 +0530

    table 5

---
+++ src/table.java
@@ -0,0 +1,14 @@
+public class table {
+   static int num=5;
+    public static void main(String[]args)
+    {
+       int i;
+        for (i=1;i<=11;i++)
+        {
+            int result = num*i;
+           // System.out.printf("%d * %d=%d",num,i,result).println();
+            System.out.println(num + "*" + i + "=" + result);
+        }
+
+    }
+}
