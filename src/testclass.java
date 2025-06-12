f874aeeaa17777c1eab4d445c4c82c28083306db
mahima7080 <mahimasrivastava84@gmail.com>
2025-06-12 09:43:00 +0530

    test git push

--- src/testclass.java
+++ src/testclass.java
@@ -3,8 +3,7 @@ public class testclass {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        System.out.println("Hello and welcome Edit!");
 
     }
 }
-*
