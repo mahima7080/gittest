af90401822c365ff11dc1be48a361426214d98ab
mahima7080 <mahimasrivastava84@gmail.com>
2025-06-12 11:23:56 +0530

    addition file

---
+++ src/addition.java
@@ -0,0 +1,16 @@
+public class addition {
+
+
+    public static void main(String[] args)
+    {
+        addition aa=new addition();
+        aa.add(5,9);
+    }
+
+     void add(int a,int b)
+    {
+        int result=a+b;
+        System.out.println(result);
+    }
+}
+
