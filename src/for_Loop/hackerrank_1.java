9575f31f6db2fba16c464d4228be894a822e26bb
mahima7080 <mahimasrivastava84@gmail.com>
2025-06-17 16:00:16 +0530

    hackerrank even odd condition

---
+++ src/for_Loop/hackerrank_1.java
@@ -0,0 +1,32 @@
+package for_Loop;
+
+import java.util.Scanner;
+
+public class hackerrank_1 {
+    public static void main(String[] args){
+        System.out.println("enter the number");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        if (n%2==0)
+        {
+            if(n>=2&&n<=5)
+            {
+                System.out.println(" Not Weird");
+            }
+            else if(n>=6&&n<=20)
+            {
+                System.out.println("Weird");
+            }
+            else
+            {
+                System.out.println(" Not Weird");
+            }
+
+        }
+        else
+        {
+            System.out.println("Weird");
+        }
+
+    }
+}
