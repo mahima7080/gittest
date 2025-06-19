177ff043f52379450e7311ab9ae0f8455bc218ec
mahima7080 <mahimasrivastava84@gmail.com>
2025-06-19 13:20:05 +0530

    prime

--- src/test_abt13x/question_15.java
+++ src/test_abt13x/question_15.java
@@ -1,4 +1,23 @@
 package test_abt13x;
 
 public class question_15 {
+    public static void main(String[] args) {
+        int i, num, count;
+
+        // Checking for prime numbers
+        for (num = 1; num <= 100; num++) {
+            count = 0;
+            for (i = 2; i <= num / 2; i++) {
+                if (num % i == 0) {
+                    count++;
+                    break;
+                }
+            }
+
+
+            if (count == 0 && num != 1) {
+                System.out.println(num);
+            }
+        }
+    }
 }
