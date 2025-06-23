19abc3f9e7cfaf51af0774ae5ac262c58761b7ba
mahima7080 <mahimasrivastava84@gmail.com>
2025-06-23 18:10:46 +0530

    task palindrome String

--- src/Task_Abt13x/task_23_june/string_palindrome_or_not.java
+++ src/Task_Abt13x/task_23_june/string_palindrome_or_not.java
@@ -1,34 +1,25 @@
-//package Task_Abt13x.task_23_june;
-//
-//import java.util.Arrays;
-//
-//public class string_palindrome_or_not {
-//    public static void main(String[] args) {
-//        String str="madamh";
-//        str.length();
-//        char[] arr=str.toCharArray();
-//char[] aar2=new char[arr.length];
-//       for (int i=arr.length;i>0;i--)
-//       {
-//           aar2[arr.length-i]=arr[i-1];
-//       }
-//       if(Arrays.toString(arr).equals(Arrays.toString(aar2)))
-//       {
-//           System.out.println("palindrome");
-//       }
-//       else {
-//           System.out.println("no palindrome");
-//       }
-//    }
-//}
-
 package Task_Abt13x.task_23_june;
 
 import java.util.Arrays;
 
 public class string_palindrome_or_not {
     public static void main(String[] args) {
-
+        String str="madam";
+        char[] arr1=str.toCharArray();
+        char[] aar2=new char[arr1.length];
+       for (int i=arr1.length;i>0;i--)
+       {
+           aar2[arr1.length-i]=arr1[i-1];
+       }
+       if(Arrays.toString(arr1).equals(Arrays.toString(aar2)))
+       {
+           System.out.println("palindrome");
+       }
+       else {
+           System.out.println("no palindrome");
+       }
     }
 }
+
+
 
