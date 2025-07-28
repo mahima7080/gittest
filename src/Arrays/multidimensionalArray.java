05885156401324ba6ad43722df61aaffd1bbe740
mahima7080 <mahimasrivastava84@gmail.com>
2025-07-28 07:41:26 +0530

    hi

---
+++ src/Arrays/multidimensionalArray.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public class multidimensionalArray {
+    public static void main(String[] args) {
+        int[][] arr=new int[][]{{1,3},{5,8},{10,11}};
+        for (int[] a:arr)
+        {
+            for (int i:a)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
